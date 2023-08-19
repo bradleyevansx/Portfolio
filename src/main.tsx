b4471c2e2f00162e00478bb591eac1040a03c481
Brad <bradleyevans.x@gmail.com>
2023-08-19 16:17:15 -0400

    applying css reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import theme from "./theme";
 import React from "react";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { CSSReset, ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -8,6 +8,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <CSSReset />
       <ColorModeScript
         initialColorMode={theme.config.initialColorMode}
       ></ColorModeScript>
